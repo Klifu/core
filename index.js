86cbe659cb5feeb4681c672f20db7c42ffd4737f
Souvik <souvikde.ns@gmail.com>
2021-07-12 15:17:26 +0530

    chore: setup

--- index.js
+++
@@ -1,5 +0,0 @@
-const pkg = require('./package.json');
-
-module.exports = {
-	version: () => pkg.version,
-}
---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+class Klifu {
+	readonly _version: string = "1.0.0";
+}
+
+export default Klifu;
