05a044b4a53142c74e6a700c5a6aebfc5ca38d3e
Souvik <souvikde.ns@gmail.com>
2021-07-18 22:10:45 +0530

    feat: fixed issue with tests.

--- lib/services/rng/__tests__/service.spec.ts
+++ lib/services/rng/__tests__/service.spec.ts
@@ -4,8 +4,8 @@ import { RandomService } from '../rng.service';
 describe('Random service should ', () => {
 	it('return generated random IVs', () => {
 		let IV: IV = RandomService.generateIV();
-		expect(IV.attack).toBeLessThan(16);
-		expect(IV.defense).toBeLessThan(16);
-		expect(IV.hp).toBeLessThan(16);
+		expect(IV.attack).toBeLessThan(17);
+		expect(IV.defense).toBeLessThan(17);
+		expect(IV.hp).toBeLessThan(17);
 	})
 })
