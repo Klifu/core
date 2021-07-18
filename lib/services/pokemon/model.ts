189223fae9e895ca0f996ba8e8e03bbbacdd905c
souvik <souvikde.ns@gmail.com>
2021-07-18 13:49:50 +0530

    feat: creating Strat logic for pokemons (#16)
    * feat: creating Strat logic for pokemons
    
    * feat: created function to calculating stats
    
    * feat: updated Pokemons class to use new PokemonService
    
    * feat: add tests for pokemon service

--- lib/services/pokemon/model.ts
+++ lib/services/pokemon/model.ts
@@ -1,11 +1,32 @@
 import { pokemonListSlice } from './slices';
 import { fetchPokemons } from '../../fetcher';
 import { generateRedux } from '../../utils';
+import { PokemonBase } from './types';
+
+export class PokemonService {
+	private readonly _pokemons: PokemonBase[];
+	constructor(pokemons: PokemonBase[]) {
+		this._pokemons = pokemons;
+	}
+
+	public list() {
+		return generateRedux(pokemonListSlice(this._pokemons));
+	}
+
+	static async fetch() {
+		const { data, error } = await fetchPokemons();
+		if (error) throw error;
+		return new PokemonService(data as PokemonBase[]);
+	}
+}
 
 export class Pokemons {
 	static async list() {
-		const { data, error } = await fetchPokemons();
-		if (error) throw error;
-		return generateRedux(pokemonListSlice(data));
+		try {
+			let pokemonService: PokemonService = await PokemonService.fetch();
+			return pokemonService.list();
+		} catch (error) {
+			throw error;
+		}
 	}
 }
